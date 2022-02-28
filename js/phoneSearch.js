f79a2b69a0d44064170dfbb905851c5d66c15fd2
shufolislam1 <shufolislam1255@gmail.com>
2022-03-01 00:58:26 +0600

    without bonus part, all completed

--- js/phoneSearch.js
+++ js/phoneSearch.js
@@ -36,11 +36,37 @@ const displaySearchResult = datum =>{
             <h3 class="card-title">${datas.brand}</h3>
             <h5>${datas.phone_name}</h5
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <button type="button" class="btn btn-success">Details</button>
+            <button onclick="loadPhoneDetail('${datas.slug}')" type="button" class="btn btn-success">Details</button>
           </div>
         </div>
       </div>
         `
         searchResult.appendChild(div)
     })
+}
+
+const loadPhoneDetail = phoneId =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayPhoneDetail(data,data.data.mainFeatures))
+}
+
+const displayPhoneDetail = phone =>{
+    console.log(phone);
+    const singelResult = document.getElementById('singel-result')
+    singelResult.innerHTML = '';
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `
+    <img src="${phone.data.image}" class="card-img-top p-5" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${phone.data.brand}</h5>
+      <h6>${phone.data.releaseDate}</h6>
+      <p class="card-text">Chipset:  ${phone.data.mainFeatures.chipSet}<br>
+      Display:  ${phone.data.mainFeatures.displaySize}<br>
+      Ram:  ${phone.data.mainFeatures.memory}</p>
+    </div>
+    `
+    singelResult.appendChild(div)
 }
